f83f89211b0ee2e712f0f1210997196ad760a024
karishmas6 <carishmashukla28@gmail.com>
2023-07-19 17:24:35 +0530

    feat: get values

--- jsoncli/__init__.py
+++ jsoncli/__init__.py
@@ -1,3 +1,19 @@
 import argparse
+import json
+import os
 
-ap = argparse.ArgumentParser()
+def get_value(data, keys):
+    for key in keys:
+        if isinstance(data, dict):
+            data = data.get(key)
+        elif isinstance(data, list):
+            try:
+                key = int(key)
+                data = data[key]
+            except (IndexError, ValueError):
+                data = None
+        else:
+            return None
+        if data is None:
+            break
+    return data
