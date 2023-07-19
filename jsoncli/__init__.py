2239186010b039f7b3427cceca8b60fc1a6cf903
karishmas6 <carishmashukla28@gmail.com>
2023-07-19 20:32:53 +0530

    feat: support --type argument

--- jsoncli/__init__.py
+++ jsoncli/__init__.py
@@ -26,21 +26,30 @@ def set_value(obj, key_path, value):
         last_key = int(last_key)
     current[last_key] = value
 
-def add_value(obj, key_path, value):
+def add_value(obj, key_path, value, data_type=None):
     keys = key_path.split('.') if '.' in key_path else key_path.split('/')
     current = obj
     for key in keys[:-1]:
         if key.isdigit():
             key = int(key)
-        if key not in current:
+        if isinstance(current, list):
+            if key >= len(current):
+                current.extend([{}] * (key - len(current) + 1))
+            current = current[key]
+        elif key not in current:
             current[key] = {} if keys[keys.index(key) + 1].isdigit() else {}
         current = current[key]
+
     last_key = keys[-1]
     if last_key.isdigit():
         last_key = int(last_key)
-    if last_key in current:
-        raise ValueError(f"Key '{last_key}' already exists.")
-    current[last_key] = value
+
+    if isinstance(current, list):
+        if last_key >= len(current):
+            current.extend([{}] * (last_key - len(current) + 1))
+        current[last_key] = convert_to_data_type(value, data_type)
+    else:
+        current[last_key] = convert_to_data_type(value, data_type)
 
 def delete_value(obj, key_path):
     keys = key_path.split('.') if '.' in key_path else key_path.split('/')
@@ -64,13 +73,30 @@ def validate_json(json_str):
         return True
     except ValueError:
         return False
-    
+
+def convert_to_data_type(value, data_type):
+    if data_type == "string":
+        return str(value)
+    elif data_type == "integer":
+        return int(value)
+    elif data_type == "float":
+        return float(value)
+    elif data_type == "boolean":
+        return bool(value)
+    elif data_type == "null":
+        return None
+    elif data_type == "object":
+        return json.loads(value)
+    else:
+        return value
+
 def main():
     parser = argparse.ArgumentParser(description="Command-line utility for manipulating JSON files.")
     parser.add_argument("command", choices=["get", "set", "add", "delete", "validate"], help="Specify the command")
     parser.add_argument("file", help="Path to the JSON file")
     parser.add_argument("key_path", nargs="?", help="Dot or slash separated key path for get, set, and add commands")
     parser.add_argument("value", nargs="?", help="Value for set and add commands")
+    parser.add_argument("--type", choices=["string", "integer", "float", "boolean", "null", "object"], help="Specify the data type of the new value")
     args = parser.parse_args()
 
     with open(args.file, "r") as f:
@@ -82,14 +108,15 @@ def main():
         print(value)
     elif args.command == "set":
         key_path = args.key_path
-        value = args.value  # No need to convert value to JSON here
+        value = args.value
         set_value(data, key_path, value)
         with open(args.file, "w") as f:
             json.dump(data, f, indent=2)
     elif args.command == "add":
         key_path = args.key_path
-        value = args.value  # No need to convert value to JSON here
-        add_value(data, key_path, value)
+        value = args.value
+        data_type = args.type
+        add_value(data, key_path, value, data_type)
         with open(args.file, "w") as f:
             json.dump(data, f, indent=2)
     elif args.command == "delete":
@@ -104,6 +131,6 @@ def main():
             print("Valid JSON.")
         else:
             print("Invalid JSON.")
-     
+
 if __name__ == "__main__":
     main()
