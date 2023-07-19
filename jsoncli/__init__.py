a33db43545ad1fa812b9dd1c986cb145e29066fd
karishmas6 <carishmashukla28@gmail.com>
2023-07-19 17:25:14 +0530

    feat: cli commands

--- jsoncli/__init__.py
+++ jsoncli/__init__.py
@@ -17,3 +17,11 @@ def get_value(data, keys):
         if data is None:
             break
     return data
+
+def main():
+    parser = argparse.ArgumentParser(description="⚡ Manipulate JSON files through CLI ⚡")
+    parser.add_argument("command", choices=["get"], help="Command to perform")
+    parser.add_argument("file", help="JSON file to manipulate")
+    parser.add_argument("keys", nargs="*", help="Keys to target the value")
+    parser.add_argument("--value", help="Value to set or add")
+    args = parser.parse_args()
